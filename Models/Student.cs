7945448c061c78e9b92f99d607949aa3d17118e3
nightSanches <galesavich@gmail.com>
2024-06-15 08:28:21 +0500

    Разработанные модели проекта. Савичев

---
+++ Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr52savichev.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int IdGroup { get; set; }
+        public bool Expellend { get; set; }
+        public DateTime DateExpelled { get; set; }
+        public Student(int Id, string Firstname, string Lastname, int IdGroup, bool Expellend, DateTime DateExpelled)
+        {
+            this.Id = Id;
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.IdGroup = IdGroup;
+            this.Expellend = Expellend;
+            this.DateExpelled = DateExpelled;
+        }
+    }
+}

ed82ee8da1c88bd5a7f064031e254924eda5d736
nightSanches <galesavich@gmail.com>
2024-06-15 08:53:46 +0500

    Revert "Программирование окна Main. Савичев"
    This reverts commit 8f2329833f7dbbe6e9fae203fd07cc15f702d8c8.

--- Items/Student.xaml.cs
+++ Items/Student.xaml.cs
@@ -1,6 +1,4 @@
-﻿using pr52savichev.Classes;
-using pr52savichev.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,56 +20,9 @@ namespace pr52savichev.Items
     /// </summary>
     public partial class Student : UserControl
     {
-        public Student(StudentContext student, Main main)
+        public Student()
         {
             InitializeComponent();
-
-            TBFio.Text = $"{student.Lastname} {student.Firstname}";
-            CBExpelled.IsChecked = student.Expellend;
-            List<DisciplineContext> StudentDiscplines = main.AllDisciplines.FindAll(x => x.IdGroup == student.IdGroup);
-
-            int NecessarilyCount = 0;
-            int WorksCount = 0;
-            int DoneCount = 0;
-            int MissedCount = 0;
-
-            foreach (DisciplineContext StudentDiscipline in StudentDiscplines)
-            {
-                List<WorkContext> StudentWorks = main.AllWorks.FindAll(x => (x.IdType == 1 || x.IdType == 2 ||
-                            x.IdType == 3) && x.IdDiscipline == StudentDiscipline.Id);
-                NecessarilyCount += StudentWorks.Count;
-
-                foreach (WorkContext WorkContext in StudentWorks)
-                {
-                    EvaluationContext evaluation = main.AllEvaluation.Find(x =>
-                        x.IdWork == WorkContext.Id &&
-                        x.IdStudent == student.Id);
-                    if (evaluation != null && evaluation.Value.Trim() != "" && evaluation.Value != "2")
-                    {
-                        DoneCount++;
-                    }
-                }
-
-                StudentWorks = main.AllWorks.FindAll(x =>
-                    (x.IdType != 4 && x.IdType != 5) && x.IdDiscipline == StudentDiscipline.Id);
-                WorksCount += StudentWorks.Count;
-
-                foreach (WorkContext StudentWork in StudentWorks)
-                {
-                    EvaluationContext Evaluation = main.AllEvaluation.Find(x =>
-                        x.IdWork == StudentWork.Id &&
-                        x.IdStudent == student.Id);
-
-                    if (Evaluation != null && Evaluation.Lateness.Trim() != "")
-                    {
-                        MissedCount += Convert.ToInt32(Evaluation.Lateness);
-                    }
-                }
-
-                doneWorks.Value = (100f / (float)NecessarilyCount) * ((float)DoneCount);
-                missedCount.Value = 100f / ((float)WorksCount * 90) * ((float)MissedCount);
-                TBGroup.Text = main.AllGroups.Find(x => x.Id == student.IdGroup).Name;
-            }
         }
     }
 }
